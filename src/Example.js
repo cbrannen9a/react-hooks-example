c356a3bcee60e9db5c3a299d97431d982e20dcca
Chris Brannen <cbrannen@outlook.com>
2018-11-12 17:21:18 +0000

    Adding a second hook

--- src/Example.js
+++ src/Example.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Example() {
 	// Declare a new state variable, which we'll call "count"
 	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		// Update the document title using the browser API
+		document.title = `You have written ${count} hooks`;
+	});
 
 	return (
 		<div>
 			<p>You have written {count} hooks</p>
 			<button onClick={() => setCount(count + 1)}>
 				Click me
-      </button>
+     		</button>
 		</div>
 	);
 }
